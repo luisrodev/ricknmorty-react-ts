ff6373bf7e0a4deec1c594e651300ce2bdc4dbc2
Luis Rubio <luirting@gmail.com>
2021-11-04 15:48:03 -0700

    Create character item component with his styles

--- src/character/components/CharacterGrid/index.tsx
+++ src/character/components/CharacterGrid/index.tsx
@@ -1,26 +1,16 @@
-import { Link } from "react-router-dom";
 import { Character } from "../../types";
+import CharacterItem from "../CharacterItem";
 import "./index.css";
 
 interface Props {
   characters: Character[];
 }
 
-const size: number = 120;
-
 const CharacterGrid = ({ characters }: Props) => {
   return (
     <div className="character-grid-container">
-      {characters.map(({ id, image, name }) => (
-        <div className="character-box" key={id}>
-          <Link to={`/ricknmorty/${id}`}>
-            <div className="character-container">
-              <img width={size} height={size} src={image} alt={name} />
-              {name}
-            </div>
-          </Link>
-          {/* <p title={name}>{name}</p> */}
-        </div>
+      {characters.map((character) => (
+        <CharacterItem character={character} />
       ))}
     </div>
   );
